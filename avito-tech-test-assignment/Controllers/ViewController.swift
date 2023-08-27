fd03a00f535123b001bd6fce7b1ff865fc9893bf
Elina Karapetian <elina.karapetyan.00@mail.ru>
2023-08-27 17:22:45 +0400

    created a file structure according to MVC pattern

---
+++ avito-tech-test-assignment/Controllers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  avito-tech-test-assignment
+//
+//  Created by Элина Карапетян on 25.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
